52d23ca2d07db6265b230a01063787568d16d680
李晏瑋 <magic556679@gmail.com>
2022-12-11 21:25:10 +0800

    feat posts router add createPosts and deleteAll Posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,7 @@ var router = express.Router();
 const PostsControllers = require('../controllers/posts');
 
 router.get('/', PostsControllers.getPosts);
+router.post('/', PostsControllers.createdPosts);
+router.delete('/', PostsControllers.deleteAllPosts);
 
 module.exports = router;
