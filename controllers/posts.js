5ac9847229ded1d9efa408e1257833dfe95d55b7
李晏瑋 <magic556679@gmail.com>
2022-12-11 21:23:36 +0800

    feat 增加 新增貼文、刪除所有貼文 api

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,41 @@
 const Posts = require('../models/postsModel');
-const appError = require("../service/appError");
+const User = require('../models/usersModel');
+const handleError = require("../service/handleError");
+const handleSuccess = require("../service/handleSuccess");
 
 
 const posts = {
   async getPosts(req, res) {
     const allPosts = await Posts.find();
-    res.send({
-      status: true,
-      message: allPosts
-    });
-    res.end();
-		// handleSuccess(res, allPosts);
+    handleSuccess(res, allPosts);
+  },
+  async createdPosts(req, res, next) {
+    try {
+      const { name ,content, tags, type } = req.body
+      // user Collections id
+      // const id = '639443265053385cd7738da3';
+      // const searchId = await User.findById(id).exec()
+
+      if (name && content && tags && type) {
+        const newPosts = await Posts.create({
+          name: name,
+          content: content,
+          tags: tags,
+          type: type
+        });
+        handleSuccess(res, newPosts);
+        return
+      }
+      handleError(404, '檢查欄位是否都有填寫', next);
+    } catch (error) {
+      console.log(error.errors);
+      handleError(500,'程式錯誤', next);
+    }
+  },
+  async deleteAllPosts(req, res){
+    await Posts.deleteMany({})
+		const allPosts = await Posts.find();
+		handleSuccess(res, allPosts);
   },
 };
 
