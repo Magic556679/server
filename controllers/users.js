cb480230a1cddeb4ebd685b866b002a6c1a34c33
李晏瑋 <magic556679@gmail.com>
2023-02-16 23:22:06 +0800

    feat 驗證 email 是否重複、重構 register function

--- controllers/users.js
+++ controllers/users.js
@@ -2,27 +2,29 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/usersModel');
 const appError = require('../service/appError');
 const handleSuccess = require('../service/handleSuccess');
+const handleErrorAsync = require('../service/handleErrorAsync');
 
 const user = {
   async getAllUsers(req, res) {
     const allUsers = await User.find();
     handleSuccess(res, allUsers);
   },
-  async register(req, res, next) {
-    try {
-      const { name, email } = req.body;
-      if(!name || !email) {
-        return appError('400', '欄位填寫錯誤', next)
+  register: handleErrorAsync(async(req, res, next) => {
+      const { name, email, password } = req.body;
+      if(!name || !email || !password) {
+        return next(appError('400', '欄位填寫錯誤', next))
+      };
+      const hasEmail = await User.findOne({ email })
+      if (hasEmail) {
+        return next(appError('400', 'E-mail 已被註冊', next))
       };
       const createUser = await User.create({
         name: name,
-        email: email
+        email: email,
+        password: password
       });
       handleSuccess(res, createUser);
-    } catch (error) {
-      appError(500, 'register error', next);
-    }
-  }
+  })
 };
 
 module.exports = user;
