c364b431a72b88ad1687a258c787d318133353b1
李晏瑋 <magic556679@gmail.com>
2023-04-29 11:40:50 +0800

    feat create update profile,password router

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const UsersControllers = require('../controllers/users');
+const { isAuth } = require('../service/auth');
 
 router.get('/', UsersControllers.getAllUsers);
 router.get('/profile', UsersControllers.profile);
-router.patch('/profile', UsersControllers.updateProfile);
+router.patch('/profile',  isAuth, UsersControllers.updateProfile);
+router.patch('/updatePassword',  isAuth, UsersControllers.updatePassword);
 router.post('/register', UsersControllers.register);
 router.post('/login', UsersControllers.logIn);
 
